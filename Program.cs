16c8d5e8eb87687636bfca2622cbbe51fbd283b6
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-20 08:43:24 +0200

    better explanation of indexes , starting index of action

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace _04.substrings
         static void Main(string[] args)
         {
             string text = "Pesho Gosho";
-            string substr = text.Substring(5);///remove all in text till index 5 , 
-            //and gets all from index 6 till end of text
+            string substr = text.Substring(5);///remove all in text till index 4 , 
+            //and gets all from index 5 till end of text 
+            //gets all starting from index 5 till rest of the string
             Console.WriteLine(substr);
         }
     }
