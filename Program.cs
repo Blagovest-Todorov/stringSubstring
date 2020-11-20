e845f5d453198b0d588793e0effe9baf54794491
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-20 08:38:07 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.substrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Pesho Gosho";
+            string substr = text.Substring(5);///remove all in text till index 5 , 
+            //and gets all from index 6 till end of text
+            Console.WriteLine(substr);
+        }
+    }
+}
